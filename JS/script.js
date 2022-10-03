dbe89167e797b1adf938c021e3a23a8781a46221
unknown <juan.molina.epet20@gmail.com>
2022-10-03 11:54:05 -0300

    creando variable

--- JS/script.js
+++ JS/script.js
@@ -4,8 +4,9 @@ let suma= b + a;
 const nombre = "Molina";
 let compraJ = "alimentoP";
 var precioA = 500;
-
+const array = ["Juan Molina","Lagos Maximo","Jeremiaz Lopez", "Franco Mendez"];
     console.log(suma);
     console.log(nombre);
     console.log(compraJ);
     console.log(precioA);
+
