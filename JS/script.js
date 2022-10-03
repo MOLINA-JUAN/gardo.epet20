fc53a3660621dc139cad9c78ffc1d4634af06c72
unknown <juan.molina.epet20@gmail.com>
2022-10-03 11:55:23 -0300

    muestro el array

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,9 @@ const nombre = "Molina";
 let compraJ = "alimentoP";
 var precioA = 500;
 const array = ["Juan Molina","Lagos Maximo","Jeremiaz Lopez", "Franco Mendez"];
+
     console.log(suma);
     console.log(nombre);
     console.log(compraJ);
     console.log(precioA);
-
+    console.log(array);
