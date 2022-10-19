6ace4c9003fe3575260376458eca57556ce2ef0a
unknown <juan.molina.epet20@gmail.com>
2022-10-19 15:38:36 -0300

    agrego un class al div del copyrigth

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,36 @@
-var a = 5;
-var b = 6;
-let suma= b + a;
+//#region 
 const nombre = "Molina";
-let compraJ = "alimentoP";
+let compraJ = "alimento";
 var precioA = 500;
-const array = ["Juan Molina","Lagos Maximo","Jeremiaz Lopez", "Franco Mendez"];
-
-    console.log(suma);
+let array = [`Juan Molina`,`Lagos Maximo`,`Jeremiaz Lopez`, `Franco Mendez`];
+var an = 6;
+var bn = 8;
+//#endregion
+//#region 
+function sumar (an,bn){
+    return an + bn;
+}
+function restar (an,bn){
+    return an - bn;
+}
+function div (an,bn){
+    return an / bn;
+}
+function mul (an,bn){ //funcion de la multiltiplicacion
+    return an * bn;
+}
+//#endregion
+//#region 
+    console.log(sumar(an,bn));
+    console.log(restar(an,bn));
+    console.log(div(an,bn));
+    console.log(mul(an,bn));
     console.log(nombre);
     console.log(compraJ);
     console.log(precioA);
-    console.log(array);
+//#endregion
+//#region 
+    for(x=0;x<4;x++){
+        console.log(array[x]);
+    }
+//#endregion
