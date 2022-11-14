0fcb969538ce3fbdd41488ece327dcddce31d8e2
unknown <juan.molina.epet20@gmail.com>
2022-11-14 13:50:29 -0300

    representamos los nodos o llamamos estos nodos, aplicacmos el h1 al div y llamamos a selector de rotacion que esta en js

--- JS/script.js
+++ JS/script.js
@@ -4,8 +4,7 @@ var capa = document.getElementById("capa");
     const input =document.getElementById('input').value;
     const h1 = document.createElement('h1');
     console.log(input);
-    h1.innerHTML = texto;
-
+    h1.textContent=input;
     capa.appendChild(h1);
-    
+    capa.classList.add('rotacion');
     }
