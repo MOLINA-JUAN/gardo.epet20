73debdaf111be6ab9815c1e4f9e137a8af2710a7
unknown <juan.molina.epet20@gmail.com>
2022-11-14 13:31:19 -0300

    pruebo asignar variables

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 var capa = document.getElementById("capa");
     
     function addElemento(){
+    const input =document.getElementById('input').value;
+    var h1 = document.createElement('h1');
     
-    var h1 = document.createElement("h1");
-
     h1.innerHTML = texto;
 
     capa.appendChild(h1);
