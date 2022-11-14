712cdaed8061bb21c76d45c181279bebe6de8320
unknown <juan.molina.epet20@gmail.com>
2022-11-14 13:28:54 -0300

    borro codigo que no quiero utilizar

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,11 @@
 var capa = document.getElementById("capa");
     
-    function addElemento (texto){
-
-    var capa = document.getElementById("capa");
-
+    function addElemento(){
+    
     var h1 = document.createElement("h1");
 
     h1.innerHTML = texto;
 
     capa.appendChild(h1);
-    body.classList.add("")
+    
     }
