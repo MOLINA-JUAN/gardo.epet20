a90bec96f96efe1aa6f033b8fe1d7268151f9f0a
unknown <juan.molina.epet20@gmail.com>
2022-11-14 13:42:11 -0300

    declaro variables input h1 e input, y las llamo

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,8 @@ var capa = document.getElementById("capa");
     
     function addElemento(){
     const input =document.getElementById('input').value;
-    var h1 = document.createElement('h1');
-    
+    const h1 = document.createElement('h1');
+    console.log(input);
     h1.innerHTML = texto;
 
     capa.appendChild(h1);
