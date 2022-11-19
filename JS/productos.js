2f64193bd3923611fa1f92f7ce987fbf4c374b7b
LopezJeremias <lopezjeremias2004@gmail.com>
2022-11-19 15:57:51 -0300

    Agregando productos

---
+++ JS/productos.js
@@ -0,0 +1,11 @@
+const stockProductos = [
+    {
+        Id: 1,
+        nombre: "collar y correa",
+        cantidad: 1,
+        desc: "collar y correa para pasear mascotas",
+        precio: 800,
+        img: "IMAGENES/collar y correa.webp",
+    },
+    
+]
