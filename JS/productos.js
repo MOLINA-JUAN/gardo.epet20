bc8b964572ede4f65f07276797169a1b75581abd
LopezJeremias <lopezjeremias2004@gmail.com>
2022-11-19 15:58:49 -0300

    Agregando producto 2 en js

--- JS/productos.js
+++ JS/productos.js
@@ -7,5 +7,12 @@ const stockProductos = [
         precio: 800,
         img: "IMAGENES/collar y correa.webp",
     },
-    
+    {
+        Id: 2,
+        nombre: "cucha para gatos",
+        cantidad: 1,
+        desc: "cucha para que sus gatos duerman comodos",
+        precio: 2500,
+        img: "IMAGENES/cucha-gatos.jfif",
+    },
 ]
