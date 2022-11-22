8477ede7dcdb662c374693333df72022e2640a6e
LopezJeremias <lopezjeremias2004@gmail.com>
2022-11-22 19:47:47 -0300

    Agregando cambios en html y js

--- JS/productos.js
+++ JS/productos.js
@@ -74,7 +74,7 @@ const stockProductos = [
     document.addEventListener('DOMContentLoaded', () =>{
     carrito = JSON.parse(localStorage.getItem('carrito')) || []
     mostrarcarrito();
-    document.querySelector("#activarFuncion").click(procesarPedido);
+    document.querySelector("activarFuncion").click(procesarPedido);
     });
 
     if(formulario){
